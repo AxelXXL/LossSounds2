ef13406251909cda3ecd9b488cd13e4c1865023a
R1c4rdoPC <117680522+R1c4rdoPC@users.noreply.github.com>
2023-10-23 18:16:48 -0600

    Se agrego html y css pagina index (Ricardo)

--- LossSounds/App_Start/BundleConfig.cs
+++ LossSounds/App_Start/BundleConfig.cs
@@ -23,7 +23,6 @@ namespace LossSounds
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
